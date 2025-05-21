3b6ef7c55da3acc7663f897005eb2707bba5eba3
hossam <habdelrazek2198@gmail.com>
2025-05-21 03:13:39 +0300

    isolated the preprocessing

--- src/train.py
+++ src/train.py
@@ -5,7 +5,6 @@ for bank consumer churn prediction.
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.compose import make_column_transformer
@@ -18,97 +17,8 @@ from sklearn.metrics import (
     confusion_matrix,
     ConfusionMatrixDisplay,
 )
-
-### Import MLflow
-
-def rebalance(data):
-    """
-    Resample data to keep balance between target classes.
-
-    The function uses the resample function to downsample the majority class to match the minority class.
-
-    Args:
-        data (pd.DataFrame): DataFrame
-
-    Returns:
-        pd.DataFrame): balanced DataFrame
-    """
-    churn_0 = data[data["Exited"] == 0]
-    churn_1 = data[data["Exited"] == 1]
-    if len(churn_0) > len(churn_1):
-        churn_maj = churn_0
-        churn_min = churn_1
-    else:
-        churn_maj = churn_1
-        churn_min = churn_0
-    churn_maj_downsample = resample(
-        churn_maj, n_samples=len(churn_min), replace=False, random_state=1234
-    )
-
-    return pd.concat([churn_maj_downsample, churn_min])
-
-
-def preprocess(df):
-    """
-    Preprocess and split data into training and test sets.
-
-    Args:
-        df (pd.DataFrame): DataFrame with features and target variables
-
-    Returns:
-        ColumnTransformer: ColumnTransformer with scalers and encoders
-        pd.DataFrame: training set with transformed features
-        pd.DataFrame: test set with transformed features
-        pd.Series: training set target
-        pd.Series: test set target
-    """
-    filter_feat = [
-        "CreditScore",
-        "Geography",
-        "Gender",
-        "Age",
-        "Tenure",
-        "Balance",
-        "NumOfProducts",
-        "HasCrCard",
-        "IsActiveMember",
-        "EstimatedSalary",
-        "Exited",
-    ]
-    cat_cols = ["Geography", "Gender"]
-    num_cols = [
-        "CreditScore",
-        "Age",
-        "Tenure",
-        "Balance",
-        "NumOfProducts",
-        "HasCrCard",
-        "IsActiveMember",
-        "EstimatedSalary",
-    ]
-    data = df.loc[:, filter_feat]
-    data_bal = rebalance(data=data)
-    X = data_bal.drop("Exited", axis=1)
-    y = data_bal["Exited"]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=1912
-    )
-    col_transf = make_column_transformer(
-        (StandardScaler(), num_cols), 
-        (OneHotEncoder(handle_unknown="ignore", drop="first"), cat_cols),
-        remainder="passthrough",
-    )
-
-    X_train = col_transf.fit_transform(X_train)
-    X_train = pd.DataFrame(X_train, columns=col_transf.get_feature_names_out())
-
-    X_test = col_transf.transform(X_test)
-    X_test = pd.DataFrame(X_test, columns=col_transf.get_feature_names_out())
-
-    # Log the transformer as an artifact
-
-    return col_transf, X_train, X_test, y_train, y_test
+from preprocessing import preprocess
+import mlflow
 
 
 def train(X_train, y_train):
@@ -137,38 +47,43 @@ def train(X_train, y_train):
 
 def main():
     ### Set the tracking URI for MLflow
-
+    mlflow.set_tracking_uri("https://localhost:5000")
     ### Set the experiment name
-
+    mlflow.set_experiment("Experiment_1")
 
     ### Start a new run and leave all the main function code as part of the experiment
+    with mlflow.start_run():
+        ### Log the run ID
+        run_id = mlflow.active_run().info.run_id
+        print(f"Run ID: {run_id}")
+        df = pd.read_csv("data/Churn_Modelling.csv")
+        col_transf, X_train, X_test, y_train, y_test = preprocess(df)
 
-    df = pd.read_csv("data/Churn_Modelling.csv")
-    col_transf, X_train, X_test, y_train, y_test = preprocess(df)
+        log_artifact("transformer.pkl", col_transf)
 
-    ### Log the max_iter parameter
+        ### Log the max_iter parameter
 
-    model = train(X_train, y_train)
+        model = train(X_train, y_train)
 
-    
-    y_pred = model.predict(X_test)
+        
+        y_pred = model.predict(X_test)
 
-    ### Log metrics after calculating them
-
-
-    ### Log tag
+        ### Log metrics after calculating them
 
 
-    
-    conf_mat = confusion_matrix(y_test, y_pred, labels=model.classes_)
-    conf_mat_disp = ConfusionMatrixDisplay(
-        confusion_matrix=conf_mat, display_labels=model.classes_
-    )
-    conf_mat_disp.plot()
-    
-    # Log the image as an artifact in MLflow
-    
-    plt.show()
+        ### Log tag
+
+
+        
+        conf_mat = confusion_matrix(y_test, y_pred, labels=model.classes_)
+        conf_mat_disp = ConfusionMatrixDisplay(
+            confusion_matrix=conf_mat, display_labels=model.classes_
+        )
+        conf_mat_disp.plot()
+        
+        # Log the image as an artifact in MLflow
+        
+        plt.show()
 
 
 if __name__ == "__main__":
